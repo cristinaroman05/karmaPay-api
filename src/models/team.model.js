b8fe1a9b63d9d34609e9d9215da864db0e851b93
Cristina Roman <cristinaroman05.cr@gmail.com>
2025-02-16 14:59:19 +0100

    añadido propiedad category a team

--- src/models/team.model.js
+++ src/models/team.model.js
@@ -15,17 +15,18 @@ const selectByName = async (teamName) => {
     if (result.length === 0) return null
     return result[0]
 }
-const addTeam = async ({ name, description }) => {
-    const [result] = await pool.query("insert into teams (name, description, img) values(?, ?, ?)", [name, description, teamImage])
+const addTeam = async ({ name, description, category }) => {
+    const [result] = await pool.query("insert into teams (name, description, category, img) values(?, ?, ?, ?)", [name, description, category, teamImage])
     return result
 }
-const updateById = async (teamId, { name, description, img }) => {
+const updateById = async (teamId, { name, description, category, img }) => {
     const [result] = await pool.query(
-        "update teams set name = ?, description = ?, img = ? where id = ?",
+        "update teams set name = ?, description = ?, img = ?, category = ? where id = ?",
         [
             name,
             description,
             img,
+            category,
             teamId
         ]
     )
