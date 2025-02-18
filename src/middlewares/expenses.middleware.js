96955d5c96b98280360036a49f7f3b3014dfe043
AlexCastetPedrosa <alexcasteto03@gmail.com>
2025-02-18 14:55:10 +0100

    middleware expense

---
+++ src/middlewares/expenses.middleware.js
@@ -0,0 +1,27 @@
+const Expenses = require('../models/expense.model');
+//     `, [userId]);
+const checkUserIDCreator = async (req, res, next) => {
+    const creator = req.params.id;
+    if (creator !== req.user.id) {
+        return res.status(403).json({ message: 'No estás autorizado' })
+    }
+
+    next()
+}
+
+const checkExpenseTeam = async (req, res, next) => {
+    const { idgroup } = req.params;
+    const userId = req.user.id;
+    const expenseUsers = await Expenses.selectAllByUser(userId);
+    expenseUsers.forEach(expense => {
+        if (expense.TeamID !== idgroup) {
+            return res.status(403).json({ message: `el expense ${expense.id} no pertenece a este grupo` })
+        }
+    });
+
+    
+
+    next()
+}
+
+module.exports = { checkUserIDCreator }
