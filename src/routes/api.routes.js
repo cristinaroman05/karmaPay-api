faa4c9d34472f7744f08e1c16af9f012f997f00f
Cristina Roman <cristinaroman05.cr@gmail.com>
2025-02-18 12:42:25 +0100

    introducido el login y register en /users

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -5,6 +5,5 @@ router.use('/users', /* checkToken */ require('./api/users.routes'))
 router.use('/teams', /* checkToken */ require('./api/teams.routes'))
 router.use('/expenses', /* checkToken */ require('./api/expenses.routes'))
 
-router.use('/login', require('./api/login.routes'))
-router.use('/register', require('./api/register.routes'))
+
 module.exports = router
